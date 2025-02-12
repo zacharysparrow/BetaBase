917ce64d98bf50fd9c6102b8b9a9c48caea3172e
Zachary Sparrow <zachsparrow95@gmail.com>
2025-02-12 17:10:32 -0500

    hold id

--- mediapipe/analyze_climb.py
+++ mediapipe/analyze_climb.py
@@ -1,6 +1,6 @@
 ## TODO
 # major refactoring when everything works
-# need to find the beginning/end still points for both hands
+# better to find still points directly instad of find peaks and infer still points
 # need to do feet
 
 import pandas as pd
@@ -10,38 +10,27 @@ import matplotlib.pyplot as plt
 import cv2
 
 peak_width = 10
+hold_dist_thresh = 0.01
 
 df = pd.read_csv('pose_output.csv', dtype={'frame':int, 'landmark':str, 'x':float, 'y':float, 'z':float, 'visibility':float})
-#right_hand_data = df[df['landmark'].str.contains('RIGHT_WRIST')]
-#left_hand_data = df[df['landmark'].str.contains('LEFT_WRIST')]
 right_hand_data = df[df['landmark'].str.contains('RIGHT_INDEX')]
 left_hand_data = df[df['landmark'].str.contains('LEFT_INDEX')]
 right_foot_data = df[df['landmark'].str.contains('RIGHT_FOOT_INDEX')]
 left_foot_data = df[df['landmark'].str.contains('LEFT_FOOT_INDEX')]
 
 def calculate_velocity(coordinates, time_interval):
-  """
-  Calculates the velocity of a trajectory given a list of positions and a time interval.
-
-  Args:
-    positions: A list of tuples or lists, where each element represents the (x, y, z) coordinates of the object at a given time.
-    time_interval: The time elapsed between each position measurement.
-
-  Returns:
-    A list of tuples, where each element represents the (vx, vy, vz) velocity components at a given time.
-  """
   positions = signal.savgol_filter(np.array([tuple(r) for r in coordinates.to_numpy()]), 10, 3, axis=0)
   velocities = []
   for i in range(len(positions) - time_interval):
     delta_x = positions[i + time_interval][0] - positions[i][0]
     delta_y = positions[i + time_interval][1] - positions[i][1]
 #    delta_z = positions[i + time_interval][2] - positions[i][2]
-    delta_z = 0
+#    delta_z = 0
 
     vx = delta_x / time_interval
     vy = delta_y / time_interval
-    vz = delta_z / time_interval
-    velocities.append(np.linalg.norm((vx, vy, vz)))
+#    vz = delta_z / time_interval
+    velocities.append(np.linalg.norm((vx, vy)))
   return velocities
 
 def find_body_positions(peak_coordinates, points, peak_width):
@@ -56,27 +45,55 @@ def find_body_positions(peak_coordinates, points, peak_width):
         body_pos.append([mean_pos, np.mean(points[left_end:right_end], axis=0)])
     return body_pos
 
+def remove_similar_points(points, threshold):
+    filtered_points = []
+    for point1 in points:
+        similar_points = [point1]
+        for point2 in points:
+            if not point1.equals(point2) and np.linalg.norm(point1 - point2) < threshold:
+                similar_points.append(point2)
+                print("found dupe")
+        avg_point = np.mean(similar_points, axis=0)
+        filtered_points.append(avg_point)
+    return np.unique(np.array(filtered_points, ndmin=2), axis=0)
+
 time_interval = 3 #frames/2
-right_hand_velocities = calculate_velocity(right_hand_data[["x","y","z"]], time_interval)
-left_hand_velocities = calculate_velocity(left_hand_data[["x","y","z"]], time_interval)
+right_hand_velocities = calculate_velocity(right_hand_data[["x","y"]], time_interval)
+left_hand_velocities = calculate_velocity(left_hand_data[["x","y"]], time_interval)
 
 right_hand_move_idx = signal.find_peaks(right_hand_velocities, height=0.004, prominence=0.004, distance=peak_width)[0]
-right_hand_still_data = find_body_positions(right_hand_move_idx, right_hand_data[["x","y","z"]], peak_width)
+if np.sign(np.diff(right_hand_velocities))[0] < 0:
+    right_hand_move_idx = np.insert(right_hand_move_idx, 0, 0)
+if np.sign(np.diff(right_hand_velocities))[-1] > 0:
+    right_hand_move_idx = np.append(right_hand_move_idx, len(right_hand_data))
+
+right_hand_still_data = find_body_positions(right_hand_move_idx, right_hand_data[["x","y"]], peak_width)
 right_hand_still_idx = [i[0] for i in right_hand_still_data]
 right_hand_still_pos = [i[1] for i in right_hand_still_data]
+right_hand_still_pos = remove_similar_points(right_hand_still_pos, hold_dist_thresh)
 right_hand_still_frames = [right_hand_data.frame.iloc[i] for i in right_hand_still_idx]
 right_hand_still_vel = [right_hand_velocities[i] for i in right_hand_still_idx]
 
 left_hand_move_idx = signal.find_peaks(left_hand_velocities, height=0.004, prominence=0.004, distance=peak_width)[0]
-left_hand_still_data = find_body_positions(left_hand_move_idx, left_hand_data[["x","y","z"]], peak_width)
+if np.sign(np.diff(left_hand_velocities))[0] < 0:
+    left_hand_move_idx = np.insert(left_hand_move_idx, 0, 0)
+if np.sign(np.diff(left_hand_velocities))[-1] > 0:
+    left_hand_move_idx = np.append(left_hand_move_idx, len(left_hand_data))
+
+left_hand_still_data = find_body_positions(left_hand_move_idx, left_hand_data[["x","y"]], peak_width)
 left_hand_still_idx = [i[0] for i in left_hand_still_data]
 left_hand_still_pos = [i[1] for i in left_hand_still_data]
+left_hand_still_pos = remove_similar_points(left_hand_still_pos, hold_dist_thresh)
 left_hand_still_frames = [left_hand_data.frame.iloc[i] for i in left_hand_still_idx]
 left_hand_still_vel = [left_hand_velocities[i] for i in left_hand_still_idx]
 
-right_foot_velocities = calculate_velocity(right_foot_data[["x","y","z"]], time_interval)
-left_foot_velocities = calculate_velocity(left_foot_data[["x","y","z"]], time_interval)
+right_foot_velocities = calculate_velocity(right_foot_data[["x","y"]], time_interval)
+left_foot_velocities = calculate_velocity(left_foot_data[["x","y"]], time_interval)
 
+print("Right hand holds:")
+print(right_hand_still_pos)
+print("Left hand holds:")
+print(left_hand_still_pos)
 #plt.plot(right_hand_data.frame, right_hand_data.y, label='Right')
 #plt.plot(left_hand_data.frame, left_hand_data.y, label='Left')
 #plt.title("Hand positions")
@@ -122,12 +139,12 @@ while success:
     frame = cv2.resize(frame, (new_w, new_h))
     #Plot the points on the frame
     for p in right_hand_still_pos:
-        px = int(new_w * p.x)
-        py = int(new_h * p.y)
+        px = int(new_w * p[0])
+        py = int(new_h * p[1])
         cv2.circle(frame, [px, py], 5, (0, 0, 255))
     for p in left_hand_still_pos:
-        px = int(new_w * p.x)
-        py = int(new_h * p.y)
+        px = int(new_w * p[0])
+        py = int(new_h * p[1])
         cv2.circle(frame, [px, py], 5, (255, 0, 0))
 
     cv2.imshow('Right hand holds', frame)
